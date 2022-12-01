1092fbd3e70993888da22552161a4070768f85e3
HANG <105864471+hang2048@users.noreply.github.com>
2022-12-01 23:11:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'tangly1024', // 作者
-  BIO: 'HANG🍚', // 作者简介
+  AUTHOR: 'L🍚Z🍚H', // 作者
+  BIO: 'HANG', // 作者简介
   LINK: 'https://h-xap.top', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
